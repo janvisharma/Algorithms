0f65b8ce972c4d2ade646467ac0d5b1ac9eaea4a
janvisharma <janvi@connect.hku.hk>
2020-04-12 13:10:56 +0530

    fixes bugs

--- RecursiveAlgorithms/matrixmultiplication.cpp
+++ RecursiveAlgorithms/matrixmultiplication.cpp
@@ -9,6 +9,7 @@ void MatrixSum(vector<vector<int>>&A, vector<vector<int>>&B, vector<vector<int>>
             C[i][j] = A[i][j] + B[i][j];
         }
     }
+    
 
 }
 
@@ -21,21 +22,21 @@ void initZero(vector<vector<int>>&R, int n){
     }
 }
 
-void mergeMatrix(vector<vector<int>>C11,vector<vector<int>>C12,vector<vector<int>>C21,vector<vector<int>>C22,vector<vector<int>>&C,int n){
+void mergeMatrix(vector<vector<int>>&C11,vector<vector<int>>&C12,vector<vector<int>>&C21,vector<vector<int>>&C22,vector<vector<int>>&C,int n){
     int i,j;
     // size of matrix C is 2n
 
         // top left block  
-        for(i=0;i<n/2;i++){
-            for(j=0;j<n/2;j++){
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
                 C[i][j] = C11[i][j];
             }
         }
 
         // top right block 
-        for(i=0;i<n/2;i++){
+        for(i=0;i<n;i++){
             int k = 0;
-            for(j=n/2;j<n;j++){
+            for(j=n;j<2*n;j++){
                 int m = 0;
                 C[i][j] = C12[k][m];
                 m++;
@@ -44,9 +45,9 @@ void mergeMatrix(vector<vector<int>>C11,vector<vector<int>>C12,vector<vector<int
         }
 
         // bottom left block 
-        for(i=n/2;i<n;i++){
+        for(i=n;i<2*n;i++){
             int k = 0;
-            for(j=0;j<n/2;j++){
+            for(j=0;j<n;j++){
                 int m = 0;
                 C[i][j] = C21[k][m];
                 m++;
@@ -54,9 +55,9 @@ void mergeMatrix(vector<vector<int>>C11,vector<vector<int>>C12,vector<vector<int
             k++;
         }
         // bottom right block 
-        for(i=n/2;i<n;i++){
+        for(i=n;i<2*n;i++){
             int k = 0;
-            for(j=n/2;j<n;j++){
+            for(j=n;j<2*n;j++){
                 int m = 0;
                 C[i][j] = C22[k][m];
                 m++;
@@ -65,24 +66,19 @@ void mergeMatrix(vector<vector<int>>C11,vector<vector<int>>C12,vector<vector<int
         }
 
 }
-int multiplyMatrix(vector<vector<int>>&A, vector<vector<int>>&B){
-    return A[0][0]*B[0][0];
-}
+
 
 void SquareMatrixMultiply(vector<vector<int>>&A, vector<vector<int>>&B, vector<vector<int>>&C,int n){
-    vector<vector<int>>An(1), Bn(1);
-
     if(n==1){
         C[0][0] = A[0][0]*B[0][0];
     }
     else {
-        cout<<"statement 1";
+        
         int size = n/2;
         
         vector<vector<int>>A11(size),A12(size),A21(size),A22(size);
         vector<vector<int>>B11(size),B12(size),B21(size),B22(size);
         vector<vector<int>>C11(size),C12(size),C21(size),C22(size);
-        cout<<"statement 2";
     
         initZero(C11,n/2);
         initZero(C12,n/2);
@@ -99,8 +95,9 @@ void SquareMatrixMultiply(vector<vector<int>>&A, vector<vector<int>>&B, vector<v
         initZero(B21,n/2);
         initZero(B22,n/2);
     
-
         vector<vector<int>>TEMP1(n/2),TEMP2(n/2);
+        initZero(TEMP1,n/2);
+        initZero(TEMP2,n/2);
 
         int i, j;
         
@@ -111,8 +108,7 @@ void SquareMatrixMultiply(vector<vector<int>>&A, vector<vector<int>>&B, vector<v
                 B11[i][j] = B[i][j];
             }
         }
-        cout<<"statement 3";
- 
+      
         // top right block 
         for(i=0;i<n/2;i++){
             int k = 0;
@@ -146,34 +142,29 @@ void SquareMatrixMultiply(vector<vector<int>>&A, vector<vector<int>>&B, vector<v
             }
             k++;
         }
-        cout<<"statement 4";
+     
         SquareMatrixMultiply(A11,B11,TEMP1,n/2);
-        SquareMatrixMultiply(A12,B21,TEMP1,n/2);
+        SquareMatrixMultiply(A12,B21,TEMP2,n/2);
         MatrixSum(TEMP1,TEMP2,C11,n/2);
         initZero(TEMP1,n/2);
         initZero(TEMP2,n/2);
-        cout<<"statement 5";
-
+       
         SquareMatrixMultiply(A11,B12,TEMP1,n/2);
-        SquareMatrixMultiply(A12,B22,TEMP1,n/2);
+        SquareMatrixMultiply(A12,B22,TEMP2,n/2);
         MatrixSum(TEMP1,TEMP2,C12,n/2);
         initZero(TEMP1,n/2);
         initZero(TEMP2,n/2);
-        cout<<"statement 6";
-
+        
         SquareMatrixMultiply(A21,B11,TEMP1,n/2);
-        SquareMatrixMultiply(A22,B21,TEMP1,n/2);
+        SquareMatrixMultiply(A22,B21,TEMP2,n/2);
         MatrixSum(TEMP1,TEMP2,C21,n/2);
         initZero(TEMP1,n/2);
         initZero(TEMP2,n/2);
-        cout<<"statement 7";
-
+      
         SquareMatrixMultiply(A21,B12,TEMP1,n/2);
-        SquareMatrixMultiply(A22,B22,TEMP1,n/2);
+        SquareMatrixMultiply(A22,B22,TEMP2,n/2);
         MatrixSum(TEMP1,TEMP2,C22,n/2);
-        cout<<"statement 8";
         mergeMatrix(C11,C12,C21,C22,C,n/2);
-        cout<<"statement 9";
 
     }
 }
@@ -184,13 +175,13 @@ int main(){
     int n;
     cin>>n;
 
-    vector<vector<int>> A(2);
-    vector<vector<int>> B(2);
-    vector<vector<int>> C(2);
+    vector<vector<int>> A(n);
+    vector<vector<int>> B(n);
+    vector<vector<int>> C(n);
 
     int i, j,value;
 
-    initZero(C,2);
+    initZero(C,n);
 
     cout<<"Enter elements of array A: "<<endl;
     for(i=0;i<n;i++){
@@ -214,9 +205,8 @@ int main(){
         
     }
   
-
     SquareMatrixMultiply(A,B,C,n);
-    cout<<endl<<"the result is "<<endl;
+    cout<<endl<<"The resultant matrix is: "<<endl;
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
             cout<<C[i][j]<<" ";
