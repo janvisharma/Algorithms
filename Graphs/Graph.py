95871a17fa715f6a7bfb8499e3d5a7a3d5d9b5e7
janvisharma <janvi@connect.hku.hk>
2020-04-22 18:18:19 +0530

    adds method to find path between 2 vertices

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -64,13 +64,21 @@ class DFSPath:
     def __init__(self, G):
         self.adjListG = G.returnAdjList()
         self.stackVisited = []
-        print(self.adjListG)
+        self.vertices = G.vertices
+        self.keepTrack = [self.vertices]*self.vertices
     
     def dfs_recursive(self, s):
+        '''
+            Maintaing a keepTrack list 
+            the indexes represent the vertices of the graph
+            and the value stored at keepTrack[vertex] = parent vertex found in DFS
+            this will help us backtrack and find paths between vertices
+        '''
         self.stackVisited.append(s)
         if s in self.adjListG.keys():
             for x in self.adjListG[s]:
                 if x not in self.stackVisited:
+                    self.keepTrack[x]= s
                     self.dfs_recursive(x)
 
     def printStackVisited(self):
@@ -79,32 +87,36 @@ class DFSPath:
     def hasAPath(self,s,e):
         self.stackVisited = []
         self.dfs_recursive(s)
-        print(self.stackVisited)
-        if e in self.stackVisited:
-            print("path exists")
-            print(s, ' -> ', end='')
-            for x in self.stackVisited:
-                if x == e:
-                    print(x)
-                    break
-                else:
-                    if e in self.adjListG[x]:
-                        print(x,' -> ',end='')
-            
-            
+        # first we call DFS so that we have tracking info avail. in keepTrack list
+        path = []
+        ctr = 0
+
+        while ctr<self.vertices:
+            # processing all vertices
+            ctr = ctr + 1
+            path.append(e) # first append the 'end' 
+            # we are going to trace our way back from the end to the starting vertex
+            temp = self.keepTrack[e] # temp now holds the parent vertex of 'e'
+            e = temp 
+            if e == s:
+                path.append(e)
+                break # we will stop when we have reached the parent vertex
+        ctr = 0
+        print("\npath from end to start vertex")       
+        while ctr<len(path)-1:
+            print(path[ctr], ' -> ',end='')
+            ctr = ctr + 1
+        print(path[ctr],end=' ')
+              
 testCase = Graph(7,7)
+
 testCase.addToAdjList(1,3)
 testCase.addToAdjList(1,2)
-testCase.addToAdjList(2,9)
-testCase.addToAdjList(9,10)
-testCase.addToAdjList(9,11)
-testCase.addToAdjList(3,5)
-testCase.addToAdjList(3,6)
-testCase.addToAdjList(5,9)
-testCase.addToAdjList(5,8)
-
+testCase.addToAdjList(2,5)
+testCase.addToAdjList(2,4)
+testCase.addToAdjList(5,6)
 
 testPath = DFSPath(testCase)
-
-testPath.printStackVisited()
-testPath.hasAPath(1,8)
+testPath.hasAPath(2,3)
+testPath.hasAPath(2,4)
+testPath.hasAPath(1,6)
