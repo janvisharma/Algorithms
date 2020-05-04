de0df2dbd3dff5ad2544d9dc00f03970b1c4f01b
janvisharma <janvi@connect.hku.hk>
2020-05-04 22:16:49 +0530

    recursive approach to dominating pts prob

--- DominatingPointRecursive.cpp
+++ DominatingPointRecursive.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <algorithm> 
 using namespace std;
 struct Points{
     int x_coordinates[6];
@@ -18,14 +19,64 @@ struct Points{
 
 */
 // Sample input: (1,3) (2,2) (4,6) (7,1) (10,12) (13,20)
-void calcRanks(Points &p, int start, int end){
+
+
+void calcRanks(Points p, int start, int end){
     int size = sizeof(p.rank_coordinates)/sizeof(p.rank_coordinates[0]);
-    if(size==1){
-        p.rank_coordinates[0] = 0;
+    if(start==end){
+        cout<<"base case "<<endl;
+        p.rank_coordinates[start] = 0;
+        return;
     }
     else{
+        // cout<<"statement 1 "<<endl;
         int mid = start+(end-start)/2;
+        Points SL,SH;
+        int i;
+        for(i=0;i<mid+1;i++){
+            SL.x_coordinates[i] = p.x_coordinates[i];
+            SL.y_coordinates[i] = p.y_coordinates[i];
+            SL.rank_coordinates[i] = p.rank_coordinates[i];
+        }
+        int j = 0;
+        for(i=mid+1;i<end+1;i++){
+            SH.x_coordinates[j] = p.x_coordinates[i];
+            SH.y_coordinates[j] = p.y_coordinates[i];
+            SH.rank_coordinates[j] = p.rank_coordinates[i];
+            j = j + 1;
+        }
+        int nL = sizeof(SL.y_coordinates)/sizeof(SL.y_coordinates[0]); 
+        cout<<"size of SL is "<<nL<<endl;
         
+        // cout<<"statement 2 "<<endl;
+        calcRanks(SL, 0, mid);
+        calcRanks(SH, mid+1, end);
+    
+        cout<<"statement 3 "<<endl;
+        i = 0, j = 0;
+        
+        sort(SL.y_coordinates, SL.y_coordinates+nL);
+        cout<<"statement 4 "<<endl;
+        int nH = sizeof(SH.y_coordinates)/sizeof(SH.y_coordinates[0]); 
+        sort(SH.y_coordinates,SH.y_coordinates+nH);
+        cout<<"statement 5 "<<endl;
+        while(i<mid+1){
+            cout<<"statement 6 "<<endl;
+            if(SL.y_coordinates[i]<SH.y_coordinates[j]){
+                SH.rank_coordinates[j] += SH.rank_coordinates[j];
+                j = j + 1;
+            }
+            else{
+                if(SL.y_coordinates[i]>SH.y_coordinates[j]){
+                    i = i + 1;
+                    j = 0;
+                }
+            }
+            if(j==end+1){
+                i = i + 1;
+                j = 0;
+            }
+        }
     }
 }
 int main(){
@@ -43,6 +94,7 @@ int main(){
     }
 
     calcRanks(sampleSpace,0,5);
+
     for(int i=0;i<6;i++){
         cout<<sampleSpace.rank_coordinates[i]<<" ";
     }
