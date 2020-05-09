221039d78b9b062ed99ebb71e34682ddeaa9bb5e
janvisharma <janvi@connect.hku.hk>
2020-05-09 22:48:28 +0530

    non recursive dfs

--- Graphs/DFS.py
+++ Graphs/DFS.py
@@ -20,7 +20,7 @@ class Graph:
 
     visitedStack = []
     
-    def DFS(self,s):
+    def DFS_recursive(self,s):
         if s not in self.visitedStack:
             self.visitedStack.append(s)
         
@@ -28,11 +28,29 @@ class Graph:
             for v in self.adjList[s]:
                 if v not in self.visitedStack:
                     self.visitedStack.append(v)
-                    self.DFS(v)
+                    self.DFS_recursive(v)
         
-    
+    '''
+        DFS - non recursive solution
+        We can implement the DFS algorithm using a stack
+        Complexity: each element gets inserted into the stack once, O(V) +
+        we need to visit each edge in the AdjList so we add O(2E)
+        Total = O(V + 2E) = O(V + E) , undirected graph 
+    '''
+    stack = []
+    order = []
+    def DFS_wr(self,s):
+        self.stack.insert(0,s)
+
+        while(len(self.stack)!=0):
+            v = self.stack.pop(0)
+            self.order.append(v)
+            for u in self.adjList[v]:
+                if u not in self.order:
+                    self.stack.insert(0,u)
+
     def printDFS(self):
-        print(self.visitedStack)
+        print(self.order)
 
 
 testCase = Graph()
@@ -42,6 +60,6 @@ testCase.addToAdjList(3,5)
 testCase.addToAdjList(3,6)
 testCase.addToAdjList(6,7)
 
-testCase.DFS(2)
+testCase.DFS_wr(2)
 testCase.printDFS()
 
