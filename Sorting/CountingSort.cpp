d407fb282989ba8744b878e4c5da84438a66fe8c
janvisharma <janvi@connect.hku.hk>
2020-05-18 01:34:03 +0530

    fixes counting sort

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -1,45 +1,55 @@
 #include <iostream>
 using namespace std;
+/*
+    Sorting in Linear Time!
+    (1) The lower bound of omega(nlogn) does not hold in this case
+    because we are not using the comparison model
+    (2) Needs to be modified to handle negative numbers 
 
-void CountingSort(int A[], int n){
-    cout<<"i am in";
-    int C[6];
-    int B[n];
+    Idea: We count the number of elements that are less than or equal to 
+    a particular number in A (input) to determine its position in the sorted array
+
+    Complexity = O(n+k) where k is (max number + 1)
+    If k = O(n) then the complexity can be reduced to O(n)
+*/
+void CountingSort(int A[], int n, int k){
+    int C[k] = {0}; // don't forget to initialize with all 0s!
+    int B[n] = {0};
     int i;
-    for(i=0;i<n;i++){
-        cout<<"fault 1";
-        C[A[i]] = C[A[i]] + 1;
-    }
-    for(i=1;i<6;i++){
-        cout<<"fault 2";
-        C[i] = C[i] + C[i-1];
-    }
-    for(i=n-1;i>=0;i--){
-        cout<<"fault 3";
-        B[C[A[i]]] = A[i];
-    }
-    for(i=0;i<n;i++){
-        cout<<"fault 4";
-        A[i] = B[i];
-    }
-}
 
+    for(i=0;i<n;i++){
+        C[A[i]] = C[A[i]] + 1; // counting how many occurences of A[i] are there in input
+    }
+    
+    for(i=1;i<k;i++){
+        C[i] = C[i] + C[i-1]; // cumulative frequency
+    }
+    
+    for(i=n-1;i>=0;i--){
+        B[C[A[i]]-1] = A[i];
+        C[A[i]] = C[A[i]] - 1;
+    }
+    // output stored in B[]
+    for(i=0;i<n;i++){
+        cout<<B[i]<<" "; 
+    }
+    return;
+}
 
 int main(){
     cout<<"Enter number of elements: ";
     int n,i;
     cin>>n;
     int A[n];
-    cout<<"Enter elements of array: ";
+    cout<<"Enter range of elements you want to sort (0 to k): ";
+    int range;
+    cin>>range;
+    cout<<"Enter elements of array bt 0 to "<<range<<" : ";
     for(i=0;i<n;i++){
-        cout<<"hello"<<i<<" ";
         cin>>A[i];
     }
-    cout<<"hello";
-    CountingSort(A,n);
-    cout<<"Sorted array is ";
-    for(i=0;i<n;i++){
-        cout<<A[i]<<" ";
-    }
+    
+    CountingSort(A,n,range+1);
+    
     return 0;
 }
