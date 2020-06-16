abab830836ea6b2c09a79fbcd3f8638ca8378a6e
Janvi Sharma <janvi@connect.hku.hk>
2020-06-16 13:10:47 +0530

    Update BFS.py

--- Graphs/BFS.py
+++ Graphs/BFS.py
@@ -17,7 +17,19 @@ class Graph:
     def displayAdjList(self):
         print(self.adjList)
 
-    
+    """
+        Time Analysis
+        Since each vertex is enqueued at most once, each vertex would be dequeued once. The number of times a
+        vertex is dequeued is 1 because we keep track of visited vertices in a list [].
+        Enqueue and Dequeue take O(1) time and we perform them for each vertex once.
+        O(V+V) = O(2V) = O(V) time.
+        
+        Now we scan the adjacency list of each vertex, if the graph is undirected each edge appears twice. 
+        If the graph is directed then each edge appears once.
+        Hence time taken O(2E) = O(E)
+        
+        Total run time = O(V+E)
+    """
     def BFS(self,s):
         visited = [] # to keep track of vertices that we have visited
         queue = [] # visit breadth wise
